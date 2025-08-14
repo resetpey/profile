a6bf7aaa6411b1950452b7d804951e20992f1294
resetpey <jungheeja051508@gmail.com>
2025-08-14 17:28:35 +0900

    Add files via upload

---
+++ 강민승 포트폴리오/자바스크립트/test01new.js
@@ -0,0 +1,6 @@
+let bunho01 = 3; //값을 숫자로 할당, 변수의 자료형 숫자형 - 숫자
+console.log("bunho03 =================");
+console.log(typeof bunho01);
+            
+let bunho02 = "1"; //값을 문자로 할당, 변수의 자료형 문자형 - 문자
+console.log(typeof bunho02);
